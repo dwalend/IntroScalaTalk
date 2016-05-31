616fc830e3df21a3f13675a6ce36118437e73200
David Walend <david_walend@hms.harvard.edu>
2016-05-31 15:39:12 -0400

    Added scala-native

--- src/main/scala/net/walend/intro2scala/ProsAndCons.scala
+++ src/main/scala/net/walend/intro2scala/ProsAndCons.scala
@@ -173,7 +173,8 @@ object ProsAndCons {
       TextFragment("Compile Scala to Javascript via "),LinkFragment("scala.js","http://www.scala-js.org")),
       Style.HeadLine),
     TextLine("Run in a Browser or via node.js Instead of in the JVM",Style.TertiaryLine),
-    LinkTextLine("Project TASTY to Target Other Platforms More Easily","https://docs.google.com/document/d/1h3KUMxsSSjyze05VecJGQ5H2yh7fNADtIf3chD3_wr0/edit",Style.SupportLine),
+    LinkTextLine("Project TASTY to Target Other Platforms More Easily","https://docs.google.com/document/d/1h3KUMxsSSjyze05VecJGQ5H2yh7fNADtIf3chD3_wr0/edit",Style.HeadLine),
+    LinkTextLine("Target hardware with Scala-Native","http://www.smartjava.org/content/getting-started-scala-native",Style.TertiaryLine),
     TextLine("Sometimes Future-Proof",Style.HeadLine),
 //    LinkTextLine("Spray to use Akka streams without changing existing Spray Routes","http://spray.io/msug/#/",Style.TertiaryLine),
     TextLine("Fun",Style.HeadLine)
